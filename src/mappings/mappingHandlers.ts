614ab4fac85bc07b5a87453c4dca2a1925b7be1d
SomaSekimoto <0421.soma@gmail.com>
2022-04-06 23:11:21 +0900

    Fix: data names following schema & Cmd: yarn build

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,34 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
-
+import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types"
+import { MyObject } from "../types/models/index"
+import { Balance } from "@polkadot/types/interfaces"
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+  //Create a new MyObject with ID using block hash
+  let record = new MyObject(block.block.header.hash.toString())
+  //Record block number
+  record.blockNumber = block.block.header.number.toNumber()
+  await record.save()
 }
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
+  const {
+    event: {
+      data: [account, balance],
+    },
+  } = event
+  //Retrieve the record by its ID
+  const record = await MyObject.get(event.block.block.header.hash.toString())
+  record.account = account.toString()
+  //Big integer type Balance of a transfer event
+  record.value = (balance as Balance).toBigInt()
+  await record.save()
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
+  const record = await MyObject.get(extrinsic.block.block.header.hash.toString())
+  //Date type timestamp
+  record.timestamp = extrinsic.block.timestamp
+  //Boolean tyep
+  record.success = true
+  await record.save()
 }
-
-
